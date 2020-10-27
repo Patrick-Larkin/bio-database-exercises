18c2437ac76ec81b9c102d62d8bab9a87f6d9f9b
Patrick Larkin <patricklarkin995@gmail.com>
2020-10-27 14:41:28 -0500

    albums_migration
    feat: Edited file to different data types

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(30),
-    name VARCHAR(20),
-    release_date DATE,
-    sales INT,
-    genre VARCHAR (10),
+    name VARCHAR(75) DEFAULT 'Untitled',
+    album VARCHAR(60) NOT NULL,
+    release_date YEAR(4),
+    sales FLOAT UNSIGNED,
+    genre VARCHAR (50),
     PRIMARY KEY (id)
 );
 
